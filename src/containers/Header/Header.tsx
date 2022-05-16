cd33121eee95f7428dfbc397fdab58f0db40a900
Terry McKee <javalion@hotmail.com>
2022-05-16 15:40:26 -0500

    Add Backend API Info

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -2,6 +2,7 @@
 export const Header = () => {
     return <>
         <h1>Poetry</h1>
+        <p>Backend API Powered By: <a href="https://poetrydb.org" >https://poetrydb.org</a></p>
         <hr />
         </>
 }
